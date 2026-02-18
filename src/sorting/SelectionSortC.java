5681404ae87651ecc9922ecf70525f64b0c32501
moizusmancs <moizusmancs@gmail.com>
2026-02-18 09:00:33 +0500

    selection descending version

--- src/sorting/SelectionSortC.java
+++ src/sorting/SelectionSortC.java
@@ -46,10 +46,32 @@ public class SelectionSortC {
         }
     }
 
+    public static void selectionSortMaxDesc(int[] arr){
+
+        int n = arr.length;
+
+        for(int i=0; i<n-1; i++){
+
+            int maxIndex = i;
+
+            for (int j=i+1; j<n; j++){
+                if(arr[j] > arr[maxIndex]){
+                    maxIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[maxIndex];
+            arr[maxIndex] = temp;
+
+        }
+    }
+
     static void main() {
         int[] arr = {64,25,12,22,11};
 //        selectionSortMin(arr);
-        selectionSortMax(arr);
+//        selectionSortMax(arr);
+        selectionSortMaxDesc(arr);
 
         for(int num:arr){
             System.out.print(num + " ");
